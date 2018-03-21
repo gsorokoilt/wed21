a2ffa3fe54c4eba9eaaf0c286379d3dea1bff02d
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-03-21 17:37:00 -0400

    complete first exercise

---
+++ exercises.rb
@@ -0,0 +1,22 @@
+fav_colours = ["blue", "green", "grey"]
+cousins_ages = [45, 37, 19]
+coin_flip = ["heads", "heads", "tales"]
+fav_artists = ["Genius", "KISS", "RZA"]
+# fav_colour_symbols [:blue, :green, :grey]# to illustrate what not to do?
+
+words_defined = { :big => 'large', :ugly => 'unappealing', :short => 'lacking in stature'}
+movie_realeases = {:thor => '2009', :captain_america => '2007', :ironman => '2006' }
+city_populations = {:toronto => '3M', :new_york => '17M', :LA => '2M'}
+friends_names_ages = {:mike => '45', :sherene => '36', :toby => '77'}
+
+#exercise_1
+
+# p coin_flip                ex 1.1
+
+# p fav_colours[0]         ex 1.2
+
+# p cousins_ages.sort      ex 1.3
+
+# p cousins_ages.push  (0) ex 1.4
+
+# p movie_realeases[:thor] ex 1.5
